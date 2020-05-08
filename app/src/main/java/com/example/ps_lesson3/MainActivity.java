abab94c1622fd0693fefa9d0f92f2f969466ad98
18016375 <18016375@myrp.edu.sg>
2020-05-08 18:01:04 +0800

    activities 1-10000

--- app/src/main/java/com/example/ps_lesson3/MainActivity.java
+++ app/src/main/java/com/example/ps_lesson3/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.ps_lesson3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvC347;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvC347 = (TextView) findViewById(R.id.textViewC347);
+
+        tvC347.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                DailyCA superman = new DailyCA("Superman", 100, 60);
+                Intent i = new Intent(MainActivity.this,
+                        SecondActivity.class);
+                // Put hero object in intent
+                i.putExtra("hero", superman);
+                startActivity(i);
+            }});
+
     }
 }
